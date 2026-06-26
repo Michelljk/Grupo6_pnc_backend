ec2d9a6bbf817ec317648c234f68d69f28af8284
Michelljk <00111219@uca.edu.sv>
2026-06-26 12:29:18 -0600

    chore(enums): definir enumeradores de estado para el ciclo de vida de brackets y enfrentamientos

---
+++ src/main/java/com/example/torneos/enums/EstadoEnfrentamiento.java
@@ -0,0 +1,4 @@
+package com.example.torneos.enums;
+
+public enum EstadoEnfrentamiento { PROGRAMADO, EN_CURSO, FINALIZADO, DISPUTA}
+
---
+++ src/main/java/com/example/torneos/enums/EstadoRonda.java
@@ -0,0 +1,3 @@
+package com.example.torneos.enums;
+
+public enum EstadoRonda { PENDIENTE, EN_PROCESO, FINALIZADO}
